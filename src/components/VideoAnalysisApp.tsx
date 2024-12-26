fd294b9bd42f4494b3217bf6ec55a704b37bd242
satishgsonwane <satish.sonwane@gmail.com>
2024-12-26 11:45:55 +0530

    Rearranged Cards

--- src/components/VideoAnalysisApp.tsx
+++ src/components/VideoAnalysisApp.tsx
@@ -538,43 +538,43 @@ return (
       </div>
 
       {/* Right Column - Analysis Outputs */}
-      <div className="grid grid-cols-2 gap-4">
+      <div className="grid grid-cols-2 gap-4 h-[600px]">
         {/* ROI Output */}
-        <Card className="flex flex-col">
-          <CardHeader className="p-4">
-            <CardTitle>ROI Analysis</CardTitle>
+        <Card className="flex flex-col h-64">
+          <CardHeader className="p-2">
+            <CardTitle className="text-sm">ROI Analysis</CardTitle>
           </CardHeader>
-          <CardContent className="p-4 flex-1">
+          <CardContent className="p-2 flex-1">
             <div className="h-full bg-gray-100 rounded-lg"></div>
           </CardContent>
         </Card>
 
         {/* Canny Output */}
-        <Card className="flex flex-col">
-          <CardHeader className="p-4">
-            <CardTitle>Canny Edge Detection</CardTitle>
+        <Card className="flex flex-col h-64">
+          <CardHeader className="p-2">
+            <CardTitle className="text-sm">Canny Edge Detection</CardTitle>
           </CardHeader>
-          <CardContent className="p-4 flex-1">
+          <CardContent className="p-2 flex-1">
             <div className="h-full bg-gray-100 rounded-lg"></div>
           </CardContent>
         </Card>
 
         {/* Joint Analysis */}
-        <Card className="flex flex-col">
-          <CardHeader className="p-4">
-            <CardTitle>Joint Analysis</CardTitle>
+        <Card className="flex flex-col h-64">
+          <CardHeader className="p-2">
+            <CardTitle className="text-sm">Joint Analysis</CardTitle>
           </CardHeader>
-          <CardContent className="p-4 flex-1">
+          <CardContent className="p-2 flex-1">
             <div className="h-full bg-gray-100 rounded-lg"></div>
           </CardContent>
         </Card>
 
         {/* LOWESS Output */}
-        <Card className="flex flex-col">
-          <CardHeader className="p-4">
-            <CardTitle>LOWESS Visualization</CardTitle>
+        <Card className="flex flex-col h-64">
+          <CardHeader className="p-2">
+            <CardTitle className="text-sm">LOWESS Visualization</CardTitle>
           </CardHeader>
-          <CardContent className="p-4 flex-1">
+          <CardContent className="p-2 flex-1">
             <div className="h-full bg-gray-100 rounded-lg"></div>
           </CardContent>
         </Card>
